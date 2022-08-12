20db19b55086a79789654e0ec4df0c2d7d956ad3
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2022-08-12 05:49:51 +0500

    Complate SPrint 7

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ server.listen(PORT, () => console.log(`SERVER PORT IS ${PORT}`))
 server.get("/", (req, res) => {
     res.send({
         ok: true,
-        code: 200
+        code: 200,
+        message: "Salom dunyo"
     })
 })
