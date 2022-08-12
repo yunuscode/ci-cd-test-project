a984897f4a2a6297e965252ca9ccba739779c5cc
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2022-08-12 05:30:48 +0500

    Add express and static ser er

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = express();
+const PORT = 9321;
+
+server.listen(PORT, () => console.log(`SERVER PORT IS ${PORT}`))
+
+server.get("/", (req, res) => {
+    res.send({
+        ok: true,
+        code: 200
+    })
+})
